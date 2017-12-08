1f85951ab104c62845081aeab02fc8e744aa67e6
bryant.lee@atalent.com <bryant.lee@atalent.com>
2017-12-08 12:54:00 +0800

    add

--- app/pro/user.js
+++ app/pro/user.js
@@ -4,22 +4,48 @@ import {db_mysql} from '../db/dbconnect';
 
 
 let router = express.Router();
-    router.get('/', (req, res) => {
-        const promise = new Promise((resolve, reject)=> {
-            let db = db_mysql('select * from user');
-            resolve(db);
-        });
 
-        promise.then((value)=> {
-            let values = JSON.stringify(value);
-            values = JSON.parse(values)
-            res.send(values[0].user_pwd);
-        });
+// GET HTTPS
+router.get('/', (req, res) => {
+    const promise = new Promise((resolve, reject) => {
+        let db = db_mysql('select * from user');
+        resolve(db);
     });
 
-    router.post('/', (req, res)=> {
-        res.send(req.body);
+    promise.then((value) => {
+        let values = JSON.stringify(value);
+        values = JSON.parse(values)
+        res.send(values);
     });
+});
+
+
+// POST HTTPS
+router.post('/', (req, res) => {
+    const data = req.body;
+    let query = '';
+    if (data.handler === 1){//add
+        query = 'insert into '
+    }else if(data.handler ===2){// login
+        query = 'select * from user where (user_name = '+ data.user_name +'  or user_phone=' + data.user_name+') or user_pwd = ' + data.user_pwd;
+    }else if (data.handler ===3) {//update
+        query = 'update user set user_name = ' + data.user_name + ', user_pwd = ' +data.user_pwd + ', user_role = ' + data.role + 'where user_id =' + data.user_id
+
+    }else {// delete
+        query = 'delete from user where user_id =' + data.user_id
+    }
+    const promise = new Promise((resolve, reject) => {
+        let db = db_mysql('select * from user');
+        resolve(db);
+    });
+
+    promise.then((value) => {
+        let values = JSON.stringify(value);
+        values = JSON.parse(values)
+        res.send(values);
+    });
+    res.send(req.body);
+});
 
 module.exports = router;
 
--- www/pro/user.js
+++ www/pro/user.js
@@ -11,6 +11,8 @@ var _dbconnect = require('../db/dbconnect');
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var router = _express2.default.Router();
+
+// GET HTTPS
 router.get('/', function (req, res) {
     var promise = new Promise(function (resolve, reject) {
         var db = (0, _dbconnect.db_mysql)('select * from user');
@@ -20,11 +22,37 @@ router.get('/', function (req, res) {
     promise.then(function (value) {
         var values = JSON.stringify(value);
         values = JSON.parse(values);
-        res.send(values[0].user_pwd);
+        res.send(values);
     });
 });
 
+// POST HTTPS
 router.post('/', function (req, res) {
+    var data = req.body;
+    var query = '';
+    if (data.handler === 1) {
+        //add
+        query = 'insert into ';
+    } else if (data.handler === 2) {
+        // login
+        query = 'select * from user where (user_name = ' + data.user_name + '  or user_phone=' + data.user_name + ') or user_pwd = ' + data.user_pwd;
+    } else if (data.handler === 3) {
+        //update
+        query = 'update user set user_name = ' + data.user_name + ', user_pwd = ' + data.user_pwd + ', user_role = ' + data.role + 'where user_id =' + data.user_id;
+    } else {
+        // delete
+        query = 'delete from user where user_id =' + data.user_id;
+    }
+    var promise = new Promise(function (resolve, reject) {
+        var db = (0, _dbconnect.db_mysql)('select * from user');
+        resolve(db);
+    });
+
+    promise.then(function (value) {
+        var values = JSON.stringify(value);
+        values = JSON.parse(values);
+        res.send(values);
+    });
     res.send(req.body);
 });
 
