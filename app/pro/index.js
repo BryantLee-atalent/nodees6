ab611b3c0d6e9a9a24dbdf9f3bc5bdeb23096bb6
bryant.lee@atalent.com <bryant.lee@atalent.com>
2017-12-13 00:05:34 +0800

    add

--- app/pro/index.js
+++ app/pro/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import buffer from 'buffer';
 import {db_mysql} from '../db/dbconnect';
 let router = express.Router();
 
@@ -13,9 +14,20 @@ router.get('/', (req, res) => {
 // https post
 router.post('/', (req, res) => {
     const data = req.body.imageData;
+    const imgData = data.replace(/^data:image\/\w+;base64,/, '');
+    const dataBuffer = new buffer(imgData, 'base64');
+    const url = 'public/images/index'+ new Date().getTime() +'.png';
+    //写入文件
+    fs.writeFile(url , dataBuffer, function(err){
+        if(err){
+            res.send(err);
+        }else{
+            res.send('保存成功');
+        }
+    });
     const str = 'update index_image set image_handler = 0';
     db_mysql(str).then((value)=> {
-        const addStr = 'insert into index_image (image_src, image_handler) values( \' '+  data +'\', 1)';
+        const addStr = 'insert into index_image (image_src, image_handler) values( \' '+  url +'\', 1)';
         db_mysql(addStr).then((value2)=> {
             res.send(value2);
         });
--- www/pro/index.js
+++ www/pro/index.js
@@ -4,6 +4,10 @@ var _express = require('express');
 
 var _express2 = _interopRequireDefault(_express);
 
+var _buffer = require('buffer');
+
+var _buffer2 = _interopRequireDefault(_buffer);
+
 var _dbconnect = require('../db/dbconnect');
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
@@ -21,9 +25,20 @@ router.get('/', function (req, res) {
 // https post
 router.post('/', function (req, res) {
     var data = req.body.imageData;
+    var imgData = data.replace(/^data:image\/\w+;base64,/, '');
+    var dataBuffer = new _buffer2.default(imgData, 'base64');
+    var url = 'public/images/index' + new Date().getTime() + '.png';
+    //写入文件
+    fs.writeFile(url, dataBuffer, function (err) {
+        if (err) {
+            res.send(err);
+        } else {
+            res.send('保存成功');
+        }
+    });
     var str = 'update index_image set image_handler = 0';
     (0, _dbconnect.db_mysql)(str).then(function (value) {
-        var addStr = 'insert into index_image (image_src, image_handler) values( \' ' + data + '\', 1)';
+        var addStr = 'insert into index_image (image_src, image_handler) values( \' ' + url + '\', 1)';
         (0, _dbconnect.db_mysql)(addStr).then(function (value2) {
             res.send(value2);
         });
