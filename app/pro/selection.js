a3842d0fa935ef7634f2e9fb30749635a0d232db
bryant.lee@atalent.com <bryant.lee@atalent.com>
2017-12-21 01:32:09 +0800

    add

--- app/pro/selection.js
+++ app/pro/selection.js
@@ -5,7 +5,7 @@ let router = express.Router();
 // https get
 router.get('', (req, res) => {
     const str = '\n' +
-        'select a.mechanism_id,a.mechanism_name,a.mechanism_status,a.mechanism_reson,a.mechanism_ticket,b.small_url,b.small_endtime ,b.small_id,b.small_icon,c.forms_name,c.forms_desc,b.small_name, b.small_banner, a.mechanism_src from mechanism as a inner join small as b on a.small_id = b.small_id inner join forms as c on b.forms_id = c.forms_id';
+        'select a.mechanism_id,a.mechanism_name,a.mechanism_status,a.mechanism_reson,a.mechanism_ticket,b.small_url,b.small_endtime ,b.small_id,b.small_icon,c.forms_name,c.forms_desc,b.small_name, b.small_banner, a.mechanism_src, a.mechanism_desc from mechanism as a inner join small as b on a.small_id = b.small_id inner join forms as c on b.forms_id = c.forms_id order by a.mechanism_ticket desc';
     db_mysql(str).then((value)=> {
         res.send(value);
     });
@@ -14,7 +14,7 @@ router.get('', (req, res) => {
 // https post
 router.post('/vote', (req, res) => {
     const data = req.body;
-    const str = 'update mechanism set mechanism_status = '+ data.status +', mechanism_reson = \''+data.reson+'\'';
+    const str = 'update mechanism set mechanism_ticket = '+ data.ticket +' where mechanism_id ='+data.mechanism_id;
     db_mysql(str).then((value)=> {
         res.send(value)
     });
